90b3eccae776799519ede3f3724a6030d189f5dd
DeepL1x <potato1869@gmail.com>
2023-01-06 21:20:58 +0200

    mobile .fixed-menu position fix and general color palette change

--- script.js
+++ script.js
@@ -1,31 +1,28 @@
-function hideSignUpPopUp(){
+
+function hideSignUpPopUp() {
     const blackScreen = document.getElementsByClassName("dark-background")[0]
     blackScreen.style = "display:none;"
     const popUp = document.getElementsByClassName("sign-up-pop-up")[0]
-    popUp.style="display:none;"
+    popUp.style = "display:none;"
 }
-function showSignUpPopUp(){
+
+function showSignUpPopUp() {
     const blackScreen = document.getElementsByClassName("dark-background")[0]
     blackScreen.style = "display:block;"
     const popUp = document.getElementsByClassName("sign-up-pop-up")[0]
-    popUp.style="display:flex;"
+    popUp.style = "display:flex;"
 }
-function hideNavUpPopUp(){
+
+function hideNavUpPopUp() {
     const whiteScreen = document.getElementsByClassName("white-background")[0]
     whiteScreen.style = "display:none;"
     const popUp = document.getElementsByClassName("nav-pop-up")[0]
-    popUp.style="display:none;"
+    popUp.style = "display:none;"
 }
-function showNavPopUp(){
+
+function showNavPopUp() {
     const whiteScreen = document.getElementsByClassName("white-background")[0]
     whiteScreen.style = "display:block;"
     const popUp = document.getElementsByClassName("nav-pop-up")[0]
-    popUp.style="display:flex;"
+    popUp.style = "display:flex;"
 }
-function locateMobileMenu(){
-    const fixedMenu = document.getElementsByClassName("fixed-menu")[1]
-    let vh = document.documentElement.clientHeight;
-    fixedMenu.style("top:"+vh+"px;")
-}
-alert(window.outerHeight)
-locateMobileMenu()
